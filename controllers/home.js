8e4c2c508f73b0733e2a46ee3757297b0cb220d3
DrMalchev <dr.delyan.malchev@gmail.com>
2018-07-10 14:10:31 +0300

    First upload

---
+++ controllers/home.js
@@ -0,0 +1,19 @@
+module.exports = {
+    indexGet: (req, res) => {
+        res.render('home/index');
+    },
+    indexPost: (req, res) => {
+        let calculatorBody = req.body;
+        let calculatorParams = calculatorBody['calculator'];
+        const Calculator = require('../models/Calculator');
+        let calculator = new Calculator();
+
+        calculator.leftOperand = Number(calculatorParams.leftOperand);
+        calculator.operator = calculatorParams.operator;
+        calculator.rightOperand = Number(calculatorParams.rightOperand);
+
+        let result = calculator.calculateResult();
+        res.render('home/index', {'calculator': calculator, 'result':result});
+}
+
+};
